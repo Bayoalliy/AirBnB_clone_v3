0e0802df19772862e0b1771bdf44284f4be4fe1a
adebayo <adebayoalliy@gmail.com>
2025-06-21 05:11:23 -0700

    filedv

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """method to retrieve one object:"""
+        for obj in self.all(cls).values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):                                          """method to count the number of objects in storage:"""
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
